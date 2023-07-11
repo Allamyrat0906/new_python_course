fd2c124d67c46787f7194b404a35d3b91a24387f
Allamyrat0906 <allamyrat.annayev.2001@gmail.com>
2023-07-11 11:28:23 +0500

    new

---
+++ first.py
@@ -0,0 +1,96 @@
+name ="Allamyrat"
+
+"""
+multy comment
+
+"""
+
+# more_text="""
+# text 1
+# text2
+# """
+# print("Hi ", name)
+# print(more_text)
+
+
+# x=34
+# y=34.67
+# z="salam"
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+# intnumber=54
+# c_float=float(intnumber)
+# print(c_float)
+
+
+# import random
+
+# random_number=random.randrange(1,20)
+
+# print(random_number)
+
+# b = "Hello, World!"
+# print(b[-5:-2])
+
+
+# text="Python"
+# print("P" in text)
+
+
+# text1="       Hello"
+# print(text1.strip())
+# print(text1)
+
+# text2="Python"
+# text3=text2.index("P")
+# text4=text2.index("t")
+
+# print(text2.replace(str(text4), str(text3)))
+
+# name="Mahriban"
+# age=26
+
+# text=f"I am {name}  {age}  years old"
+# text2="I am {}  {}  years old".format(name,age)
+
+
+# print(text)
+# print(text2)
+
+# text="Python the 'best'  programming  language"
+# text1="Python the \@best\#  programming  language"
+
+# print(text)
+# print(text1)
+
+# a=int(input("Enter: "))
+# area=pow(a,2)
+# print(area)
+# import math
+# print(math.sqrt(16))
+# print(math.pi)
+# print(abs(-4))
+
+
+# begin 24
+# a=int(input("Enter:a "))
+# b=int(input("Enter:b "))
+# c=int(input("Enter:c "))
+
+# d=a
+# a=b
+# b=c
+# c=d
+
+# print("new value A", a)
+# print("new value B", b)
+# print("new value C", c)
+
+a=int(input("Enter:a  "))
+a=a*a
+a=a*a
+a=a*a
+print("new value A", a)
---
+++ zed_shaw_python/ex2.py
@@ -0,0 +1,7 @@
+# this is comment 
+
+print("Hi Python")
+
+# this comment is some text
+
+print("python is cool")
